c790a41e3c09625b8dfdaa1cfad1baefe0575f57
Dionísio <dionisioni304@gmail.com>
2025-04-20 05:34:13 +0100

    config of vue router

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,3 @@
 <template>
-  <h1>Dionisio</h1>
+    <RouterView />
 </template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,37 @@
+// CSS
 import './assets/main.css'
 
 import { createApp } from 'vue'
+
+
+
+// FONT AWESOME
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCarOn } from '@fortawesome/free-solid-svg-icons'
+
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+// VUE ROUTER
+import { createRouter, createWebHistory } from 'vue-router'
+
+import index from './project/index.vue'
+import about from './project/about.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+      { path: '/', component: index },
+      { path: '/about', component: about },
+    ],
+})
+
+
+createApp(App)
+    .component('fa', FontAwesomeIcon)
+    .use(router)
+    .mount('#app')
+
+// OTHER
