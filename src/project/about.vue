54c5a2654c9a544219e5e31505e85b878e9dc6f9
Dionísio <dionisioni304@gmail.com>
2025-04-20 05:30:38 +0100

    creat two componets of index and about to testing the vue router

---
+++ src/project/about.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <h1>About</h1>
+
+  <RouterLink to="/">Go to Home</RouterLink> | <RouterLink to="/about">Go to About</RouterLink>
+    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/project/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Home</h1>
+
+  <RouterLink to="/">Go to Home</RouterLink> | <RouterLink to="/about">Go to About</RouterLink>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
