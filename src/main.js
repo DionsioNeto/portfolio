7b48a4176df84793d0f07c770c36242d4d0b1499
Dionísio <dionisioni304@gmail.com>
2025-07-09 11:04:31 +0100

    removed fontAwesome icons

--- src/main.js
+++ src/main.js
@@ -1,24 +1,6 @@
-// CSS
-import './assets/main.css'
+import './assets/app.css'
 
 import { createApp } from 'vue'
-
-// FONT AWESOME
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { 
-  faPhone, 
-  faUser, 
-  faEnvelope, 
-  faBars,
-  faComment,
-  faEquals
-} 
-from '@fortawesome/free-solid-svg-icons'
-
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-// ONLY US THIS ICONS:
-library.add(faPhone, faUser, faEnvelope, faBars, faComment, faEquals);  
 
 import App from './App.vue'
 
@@ -26,23 +8,16 @@ import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 import index from './project/index.vue'
-import about from './project/about.vue'
 import contact from './project/contact.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
       { path: '/', component: index },
-      { path: '/about', component: about },
       { path: '/contact', component: contact },
     ],
 })
 
-
 createApp(App)
-  .component('fa', FontAwesomeIcon)
   .use(router)
   .mount('#app')
-
-// OTHER <i class="bi bi-geo-alt-fill"></i>
-// 
