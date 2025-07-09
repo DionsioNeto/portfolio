06ca1a065069cf28868a7c229361d42ccaacd366
Dionísio <dionisioni304@gmail.com>
2025-07-09 22:06:42 +0100

    The ooter done!

--- src/components/theFooter.vue
+++ src/components/theFooter.vue
@@ -1,27 +1,14 @@
-<template>
-    <footer>
-        <div></div>
-        <div> Developed by: Dionísio Neto &copy; <span id="date">2025</span></div>
-    </footer>
-    
-</template>
-<style>
-    footer{
-        background-color: #cecece;
-        padding: 15px;
-        display: flex;
-        justify-content: space-around;
-    }
-</style>
-<script>
-
-export default{
-    
-}
-
-    // let yearNow = document.getElementById('date');
-    // let now = new Date().getFullYear();
-    // yearNow.innerHTML = + now
-
-
+<script setup>
+import { Linkedin, Github, Instagram } from 'lucide-vue-next';
 </script>
+<template>
+    <footer class="container m-auto p-5 flex justify-between flex-col sm:flex-row items-center gap-5">
+        <div class="text-neutral-500 font-bold">Dionísio Neto &copy; 2022 - 2025</div>
+        <div class="text-neutral-500 font-bold">Developed by <strong>Dionísio Neto</strong></div>
+        <div class="flex gap-4">
+            <a href="https://www.linkedin.com/in/dion%C3%ADsioneto/" title="https://www.linkedin.com/in/dion%C3%ADsioneto/" target="_blank"><Linkedin  color="#4a4a4a"/></a>
+            <a href="https://github.com/DionsioNeto" title="https://github.com/DionsioNeto" target="_blank"><Github  color="#4a4a4a"/></a>
+            <a href="https://www.instagram.com/dionisio.neto14/" title="https://www.instagram.com/dionisio.neto14/" target="_blank"><Instagram  color="#4a4a4a"/></a>
+        </div>
+    </footer>
+</template>
