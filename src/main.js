3b5d59e5114a4ac712c63a6e3ea383619a33b11c
Dionísio <dionisioni304@gmail.com>
2025-08-02 18:47:44 +0100

    404, page not found

--- src/main.js
+++ src/main.js
@@ -7,12 +7,14 @@ import App from './App.vue'
 // VUE ROUTER
 import { createRouter, createWebHistory } from 'vue-router'
 
+import nf from './project/notFoundPage.vue'
 import index from './project/index.vue'
 import contact from './project/contact.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+      { path: '/:pathMatch(.*)*', name: '404', component: nf },
       { path: '/', component: index },
       { path: '/contact', component: contact },
     ],
