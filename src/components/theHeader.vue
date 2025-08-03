5a607606efa715b29fa9756abb1e375f96b84e3f
Dionísio <dionisioni304@gmail.com>
2025-08-03 03:03:26 +0100

    Header done, of now

--- src/components/theHeader.vue
+++ src/components/theHeader.vue
@@ -9,36 +9,33 @@ const tabs = [
 ] as const;
 </script>
 <template>
-  <header class="w-full border-b-1 border-b-neutral-800">
-    <div class="container m-auto flex justify-between p-4 items-center">
-      <div class="font-black text-2xl">Dionísio</div>
-      <div class="">
-        <ul class="flex items-center gap-2">
-          <li>
-            <RouterLink to="/">Home</RouterLink>
-          </li>
-          <li>
-            <RouterLink to="/contact">Contact</RouterLink>
-          </li>
-          <li>
-            <div class="inline-flex gap-1 rounded-full p-1 bg-neutral-300 dark:bg-neutral-800">
-              <button
-                  v-for="{ value, Icon } in tabs"
-                  :key="value"
-                  @click="updateAppearance(value)"
-                  :class="[
-                      'flex items-center justify-center rounded-full w-6 h-6 transition-colors',
-                      appearance === value
-                          ? 'bg-white shadow-xs dark:bg-neutral-700 dark:text-neutral-100'
-                          : 'text-neutral-500 hover:bg-neutral-200/60 hover:text-black dark:text-neutral-400 dark:hover:bg-neutral-700/60',
-                  ]"
-              >
-                  <component :is="Icon" class="h-4 w-4" />
-              </button>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
+  <header class="z-2 mx-auto w-fit rounded-full sticky py-1 px-3 backdrop-blur-xs top-3 borde-2 dark:bg-neutral-900/50 border-neutral-200 dark:border-neutral-700 shadow-lg shadow-neutral-200/50 dark:shadow-neutral-950/50">
+    <nav>
+      <ul class="flex items-center gap-2">
+        <li>
+          <RouterLink to="/">Home</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/contact">Contact</RouterLink>
+        </li>
+        <li>
+          <div class="inline-flex gap-1 rounded-full p-1 bg-neutral-100/100 dark:bg-neutral-800/100">
+            <button
+                v-for="{ value, Icon } in tabs"
+                :key="value"
+                @click="updateAppearance(value)"
+                :class="[
+                    'flex items-center justify-center rounded-full w-6 h-6 transition-colors',
+                    appearance === value
+                        ? 'bg-white shadow-xs dark:bg-neutral-700 dark:text-neutral-100'
+                        : 'text-neutral-500 hover:bg-neutral-200/60 hover:text-black dark:text-neutral-400 dark:hover:bg-neutral-700/60',
+                ]"
+            >
+                <component :is="Icon" class="h-4 w-4" />
+            </button>
+          </div>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
