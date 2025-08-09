dc8df5e8965fa2f69b51b15ca7af57005ec50b1c
Dionísio <dionisioni304@gmail.com>
2025-08-10 00:36:53 +0100

    done!

---
+++ src/project/message.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    import theHeader from '@/components/theHeader.vue';
+</script>
+<template>
+    <theHeader/>
+    <main class="flex justify-center items-center flex-col gap-3 mt-10 text-center">
+        <P class="text-6xl font-black">Sucess full 🤙🏿</P>
+        <h1 class="text-2xl font-bold">your message was sent whith sucess</h1>
+        <P class="max-w-70 text-neutral-600 font-medium">If you want to explore my personal page, click bellow 🤞🏿</P>
+        <RouterLink to="/" class="border-1 px-5 py-1 rounded-full">Go home</RouterLink>
+    </main>
+</template>
