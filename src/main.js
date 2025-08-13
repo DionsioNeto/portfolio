949ca0363ee44e3cacf8fcffd13549ddfadfa938
Dionísio <dionisioni304@gmail.com>
2025-08-13 01:28:46 +0100

    includ archive of configuration langs

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import App from './App.vue'
 // VUE ROUTER
 import { createRouter, createWebHistory } from 'vue-router'
 
+// VUE I18N
+import { i18n } from './i18n'
+
 import nf from './project/notFoundPage.vue'
 import index from './project/index.vue'
 import contact from './project/contact.vue'
@@ -26,4 +29,5 @@ const router = createRouter({
 
 createApp(App)
   .use(router)
+  .use(i18n)
   .mount('#app')
